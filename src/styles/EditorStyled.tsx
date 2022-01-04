e1dac080eca37c596e79e128639eba93e369fa3c
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-04 20:57:43 +0100

    Create editor styled component

---
+++ src/styles/EditorStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const EditorStyled = styled.textarea`
+  background-color: #8dc3d8;
+  display: block;
+  height: 250px;
+  margin: 0 auto 20px;
+  padding: 5px;
+  resize: vertical;
+  width: 60%;
+`;
+
+export default EditorStyled;
